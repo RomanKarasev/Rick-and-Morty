e4fee30e895ecf3d3929b2657014c31423f566f0
RomanKarasev <roman.karasev@icloud.com>
2022-11-10 18:13:01 +0300

    Подготовка проекта

---
+++ Rick and Morty/Controllers/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Rick and Morty
+//
+//  Created by Роман Карасёв on 10.11.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Character"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
